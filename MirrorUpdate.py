d6056ca5aac679f0a51979c9321290164851991b
Zigatronz <85059140+Zigatronz@users.noreply.github.com>
2022-09-18 14:00:21 +0800

    minor fix

--- MirrorUpdate.py
+++ MirrorUpdate.py
@@ -5,6 +5,9 @@ import os, argparse, shutil, progressbar
 def RemoveTopDir(path:str, top:str):
 	return path[top.__len__():]
 
+def GetFileExtension(filename:str):
+	return str(os.path.splitext(filename)[1]).lstrip('.').lower()
+
 # Argument
 parser = argparse.ArgumentParser(description='Backup a folder to an another folder by using mirror update method.')
 
@@ -76,46 +79,56 @@ for dirPath, dirNames, Filenames in os.walk(TargetFolder):
 			ProgBar.update(CurrentProgress)
 		# make parent folder if it isn't there
 		if not os.path.exists(DestinationFolder + RemoveTopDir(dirPath, TargetFolder)):
-			os.mkdir(DestinationFolder + RemoveTopDir(dirPath, TargetFolder))
-		# grab file extention extention
-		if not str(os.path.splitext(filename)[1]).lstrip('.').lower() in IgnoreExt:
-			if not os.path.exists(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder)):
-				# destination file not exist
-				if not UseProgressBar:
-					print("Add        : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+			if NoFail:
 				try:
-					shutil.copy2(os.path.join(dirPath, filename), DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+					os.mkdir(DestinationFolder + RemoveTopDir(dirPath, TargetFolder))
 				except:
-					print("FAILED to Add        : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
-					if not NoFail:
-						exit()
-			elif os.path.getmtime(os.path.join(dirPath, filename)) != os.path.getmtime(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder)) and not NoCModify:
-				# destination file is diffrent modify date
-				if not UseProgressBar:
-					print("Update     : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
-				try:
-					os.remove(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
-					shutil.copy2(os.path.join(dirPath, filename), DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
-				except:
-					print("FAILED to Update     : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
-					if not NoFail:
-						exit()
-			elif os.path.getsize(os.path.join(dirPath, filename)) != os.path.getsize(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder)) and not NoCSize:
-				# destination file is diffrent size
-				if not UseProgressBar:
-					print("Replace    : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
-				try:
-					os.remove(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
-					shutil.copy2(os.path.join(dirPath, filename), DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
-				except:
-					print("FAILED to Replace    : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
-					if not NoFail:
-						exit()
+					print('Can\'t create dir : ' + DestinationFolder + RemoveTopDir(dirPath, TargetFolder))
+			else:
+				os.mkdir(DestinationFolder + RemoveTopDir(dirPath, TargetFolder))
+		# ignore file extension
+		if GetFileExtension(filename) in IgnoreExt:
+			continue
+		if not os.path.exists(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder)):
+			# destination file not exist
+			if not UseProgressBar:
+				print("Add        : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+			try:
+				shutil.copy2(os.path.join(dirPath, filename), DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+			except:
+				print("FAILED to Add        : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+				if not NoFail:
+					exit()
+		elif os.path.getmtime(os.path.join(dirPath, filename)) != os.path.getmtime(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder)) and not NoCModify:
+			# destination file is diffrent modify date
+			if not UseProgressBar:
+				print("Update     : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+			try:
+				os.remove(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+				shutil.copy2(os.path.join(dirPath, filename), DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+			except:
+				print("FAILED to Update     : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+				if not NoFail:
+					exit()
+		elif os.path.getsize(os.path.join(dirPath, filename)) != os.path.getsize(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder)) and not NoCSize:
+			# destination file is diffrent size
+			if not UseProgressBar:
+				print("Replace    : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+			try:
+				os.remove(DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+				shutil.copy2(os.path.join(dirPath, filename), DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+			except:
+				print("FAILED to Replace    : " + DestinationFolder + RemoveTopDir(os.path.join(dirPath, filename), TargetFolder))
+				if not NoFail:
+					exit()
 
 # Delete Non Exist Subdir Files/Folders : Destination folder != Target folder : Del(Destination folder)
 if not NoDelete:
 	for dirPath, dirNames, Filenames in os.walk(DestinationFolder):
 		for filename in Filenames:
+			# ignore file extension
+			if GetFileExtension(filename) in IgnoreExt:
+				continue
 			if not os.path.exists(TargetFolder + RemoveTopDir(os.path.join(dirPath, filename), DestinationFolder)):
 				if os.path.exists(os.path.join(dirPath, filename)):
 					if not UseProgressBar:
